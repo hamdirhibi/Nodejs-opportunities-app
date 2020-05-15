85cc0f8011d55d8e1fdd3a65aca7e5ebfe58b726
skander <skanders@live.fr>
2020-05-15 08:19:24 +0100

    add-socket-connection-done

--- app.js
+++ app.js
@@ -1,85 +1,73 @@
-const express = require('express'); 
-const app = express (); 
-const mongoose = require('mongoose'); 
-const dotenv = require('dotenv') ; 
-const bodyParser = require('body-parser');
-const server = require('http').createServer();
-const io = require('socket.io').listen(server);
-dotenv.config() ; 
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
+dotenv.config();
 
-//import routes 
-const productRoute = require('./routes/Product'); 
-const UserRoute = require('./routes/User'); 
-const OrderRoute = require('./routes/Order'); 
-const CategoryRoute = require('./routes/Category'); 
-
-
+//import routes
+const productRoute = require("./routes/Product");
+const UserRoute = require("./routes/User");
+const OrderRoute = require("./routes/Order");
+const CategoryRoute = require("./routes/Category");
 
 // configuration of the rest API
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
-  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
+//Connect to DB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, () => {
+  console.log("connected to db ");
+});
 
-
-//Connect to DB 
-mongoose.connect(process.env.MONGODB_URI, 
-{useNewUrlParser:true}   ,
-()=>{console.log('connected to db ');  
-})
-
-   
 //    Category.findOne({
-//   name : 'juice' 
+//   name : 'juice'
 // },  (err,category)=>{
 //    Product.collection.updateMany({category : category._id}, {$set : {unit_id : 'l'}},false , true);
 // Product.collection.updateMany({category : category._id}, {$set : {unit_qte : '1'}},false , true);
 
 // })
 
- //Middlewares 
+//Middlewares
 // app.use(express.json())
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({ extended: false }));
-// route middlewares 
-app.use('/product',productRoute); 
-app.use('/user', UserRoute) ; 
-app.use('/order', OrderRoute) ; 
-app.use('/category', CategoryRoute) ; 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// route middlewares
+app.use("/product", productRoute);
+app.use("/user", UserRoute);
+app.use("/order", OrderRoute);
+app.use("/category", CategoryRoute);
 app.use("/uploads", express.static("./uploads"));
 //console.log(__dirname)
 
 // if (process.env.NODE_ENV=='production'){
-//   app.use(express.static('./client/www')); 
+//   app.use(express.static('./client/www'));
 //   app.get('*', (req, res)=>{
-//       res.sendFile(path.join(process.env.PWD,'client','www','index.html')); 
+//       res.sendFile(path.join(process.env.PWD,'client','www','index.html'));
 //   })
 
 // }
 
-io.on('connection', socket => {
+const server = require("http").createServer(app);
+server.listen(port);
+const io = require("socket.io").listen(server);
 
-  console.log("new connection made ") ; 
+io.on("connection", (socket) => {
+  console.log("new connection made ");
 
-  
-    socket.on('new-order' , data => {
-      console.log('new---order')
-      socket.emit('new-order', {order : data.order })
-  
-  })  
+  socket.on("new-order", (data) => {
+    console.log("new---order");
+    socket.emit("new-order", { order: data.order });
   });
-
-  app.listen (process.env.PORT ||3000, ()=>{
-    console.log('server up and running ');
-    }) 
-  
+});
